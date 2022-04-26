c077ad96cad24bf54c30a12cc80c90420af3442e
Timur Karimov <mathcore_detected@mail.ru>
2022-04-27 01:29:01 +0500

    Addded flash messages

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,8 +4,8 @@ class CarsController < ApplicationController
   before_action :access_action, only: %i[new create manager_cars]
 
   def index
-    @cars = Car.page(params[:page]).per(params[:per])
-    @cars = CarsFilter.new(Car.all, params).call
+    @cars = Car.page(params[:page] || 1).per(params[:per] || 50)
+    @cars = CarsFilter.new(Car.all, params).call.order(created_at: :desc)
   end
 
   def new
@@ -17,8 +17,10 @@ class CarsController < ApplicationController
     exist_car = Car.find_by(car_params)
 
     if !exist_car && @car.save
+      flash[:notice] = "Car added to database"
       redirect_to new_car_path
     elsif exist_car
+      flash[:notice] = "Car already exist in database"
       exist_car.update(record_count: exist_car.record_count + 1)
       redirect_to new_car_path
     else
